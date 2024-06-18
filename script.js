8d7d0d5c9fc9a1c4daa449562d023189bc74aa07
UsedTemplate <usedtemplate@gmail.com>
2024-06-18 13:37:00 +0200

    Updated Page Elements

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ function scrollToElement(elementSelector, instance = 0) {
 const link1 = document.getElementById("link1");
 const link2 = document.getElementById("link2");
 const link3 = document.getElementById("link3");
+const link4 = document.getElementById("link4");
 
 link1.addEventListener('click', () => {
     scrollToElement('.content', -.2);
@@ -19,6 +20,10 @@ link2.addEventListener('click', () => {
 
 link3.addEventListener('click', () => {
     scrollToElement('.column');
+});
+
+link4.addEventListener('click', () => {
+    scrollToElement('.header');
 });
 
 const cvButton = document.getElementById('cvLink');
