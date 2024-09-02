de437aef3d43c58b9213ed2f8b3c6626cec2dbc8
Tempy <usedtemplate@gmail.com>
2024-09-02 20:36:41 +0200

    Roblox page

--- script.js
+++ script.js
@@ -7,8 +7,7 @@ function scrollToElement(elementSelector, instance = 0) {
 
 const link1 = document.getElementById("link1");
 const link2 = document.getElementById("link2");
-const link3 = document.getElementById("link3");
-// const link4 = document.getElementById("link4");
+const link3 = document.getElementById("link4");
 
 link1.addEventListener('click', () => {
     scrollToElement('.content', -.2);
@@ -18,13 +17,9 @@ link2.addEventListener('click', () => {
     scrollToElement('.header');
 });
 
-link3.addEventListener('click', () => {
+link4.addEventListener('click', () => {
     scrollToElement('.column');
 });
-
-// link4.addEventListener('click', () => {
-//     scrollToElement('.header');
-// });
 
 const cvButton = document.getElementById('cvLink');
 cvButton.addEventListener('click', function() {
